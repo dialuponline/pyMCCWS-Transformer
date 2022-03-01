90541bfbc01790075f5a25a40eb3cda3b1bdf608
dialuponline <dialuponline@proton.me>
2022-03-01 11:28:21 +0000

    ath9k_htc: do some initial hardware configuration

--- make_dataset.py
+++ make_dataset.py
@@ -8,4 +8,8 @@ import collections
 from utils import get_processing_word, is_dataset_tag, make_sure_path_exists
 from fastNLP import Instance, DataSet, Vocabulary, Const
 
-def
+def expand(x):
+    sent=["<sos>"]+x[1:]+["<eos>"]
+    return [x+y for x,y in zip(sent[:-1],sent[1:])]
+    
+def read
