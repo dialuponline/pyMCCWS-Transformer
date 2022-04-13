2685135b0190fd26b3e61b3a0fb578030a4d5ae7
dialuponline <dialuponline@proton.me>
2022-04-13 11:21:47 +0000

    new test record

--- makedict.py
+++ makedict.py
@@ -1,4 +1,12 @@
 import utils
 import pickle
 import os
-from utils import is_dataset_tag, make_sure_path_e
+from utils import is_dataset_tag, make_sure_path_exists
+
+path="data/joint-sighan-simp/raw/train-all.txt"
+
+out_path="dict.pkl"
+
+dic={}
+tokens={}
+with open(path, "r", e
