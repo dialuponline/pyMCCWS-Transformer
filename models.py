9fd261a21d1930ca14f3732c4dc77d221a13ed06
dialuponline <dialuponline@proton.me>
2022-04-16 16:51:55 +0000

    Upstream merge

--- models.py
+++ models.py
@@ -3,4 +3,9 @@ import torch
 import math
 from fastNLP.modules.encoder.transformer import TransformerEncoder
 from fastNLP.modules.decoder.crf import ConditionalRandomField
-fro
+from fastNLP import Const
+import copy
+import numpy as np
+from torch.autograd import Variable
+import torch.autograd as autograd
+i
