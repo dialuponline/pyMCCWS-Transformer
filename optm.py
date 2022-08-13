c31b98fe70e1fa6ca900624ca5d3203e6018b6f5
dialuponline <dialuponline@proton.me>
2022-08-13 12:16:00 +0000

    Update search.md

--- optm.py
+++ optm.py
@@ -3,4 +3,7 @@ import torch.optim as optim
 
 class NoamOpt:
     "Optim wrapper that implements rate."
-    def __init__(sel
+    def __init__(self, model_size, factor, warmup, optimizer):
+        self.optimizer = optimizer
+        self._step = 0
+        self.warmup = warmup
