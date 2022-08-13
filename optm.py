0dbce4291abd8304677c530516f7984534d35430
dialuponline <dialuponline@proton.me>
2022-08-13 19:54:13 +0000

    Fix some lint errors

--- optm.py
+++ optm.py
@@ -15,4 +15,9 @@ class NoamOpt:
         "Update parameters and rate"
         self._step += 1
         rate = self.rate()
-        for p in se
+        for p in self.optimizer.param_groups:
+            p['lr'] = rate
+        self._rate = rate
+        self.optimizer.step()
+        
+    def rate(self, step 
