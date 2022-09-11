d047fff5f88067e5f6d7d598d34484bf693511e7
dialuponline <dialuponline@proton.me>
2022-09-11 23:04:36 +0000

    minor cosmetic fixes

--- transformer.py
+++ transformer.py
@@ -2,4 +2,7 @@ import numpy as np
 import torch
 import torch.autograd as autograd
 import torch.nn as nn
-import torch.nn.f
+import torch.nn.functional as F
+import math, copy, time
+from torch.autograd import Variable
+import matplotlib
