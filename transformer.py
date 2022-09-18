350a35e0edae5e1fc327f2555138b127f5d74ac7
dialuponline <dialuponline@proton.me>
2022-09-18 08:57:40 +0000

    Minor/ignore - Formatting

--- transformer.py
+++ transformer.py
@@ -9,4 +9,9 @@ import matplotlib.pyplot as plt
 
 def clones(module, N):
     "Produce N identical layers."
-    return nn.ModuleList([copy.deepcopy(module) for 
+    return nn.ModuleList([copy.deepcopy(module) for _ in range(N)])
+    
+def subsequent_mask(size):
+    "Mask out subsequent positions."
+    attn_shape = (1, size, size)
+    subsequen
