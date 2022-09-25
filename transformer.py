c5727c43f1aa69d8ac5d36e01d6f343ef2d9b210
dialuponline <dialuponline@proton.me>
2022-09-25 19:00:19 +0000

    Update version info

--- transformer.py
+++ transformer.py
@@ -23,4 +23,7 @@ def attention(query, key, value, mask=None, dropout=None):
     scores = torch.matmul(query, key.transpose(-2, -1)) / math.sqrt(d_k)
     if mask is not None:
         #print(scores.size(),mask.size())
-        
+        scores = scores.masked_fill(mask == 0, -1e9)
+    p_attn = F.softmax(scores, dim = -1)
+    if dropout is not None:
+        p_
