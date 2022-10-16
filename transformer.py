f4c1566a9714436e4ce60120485648b79c65d013
dialuponline <dialuponline@proton.me>
2022-10-16 12:42:49 +0000

    ipmi: Reset the KCS timeout when starting error recovery

--- transformer.py
+++ transformer.py
@@ -67,4 +67,5 @@ class LayerNorm(nn.Module):
     "Construct a layernorm module (See citation for details)."
     def __init__(self, features, eps=1e-6):
         super(LayerNorm, self).__init__()
-       
+        self.a_2 = nn.Parameter(torch.ones(features))
+        self.b_2 = nn.Parameter(torch.zeros(features))
