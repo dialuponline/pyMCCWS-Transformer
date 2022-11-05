e1c8624d598a122e05c133cdb53b5a0ba0b04fc3
dialuponline <dialuponline@proton.me>
2022-11-05 22:36:52 +0000

    sync with transifex

--- transformer.py
+++ transformer.py
@@ -74,4 +74,7 @@ class LayerNorm(nn.Module):
     def forward(self, x):
         mean = x.mean(-1, keepdim=True)
         std = x.std(-1, keepdim=True)
-        
+        return self.a_2 * (x - mean) / (std + self.eps) + self.b_2
+
+class PositionwiseFeedForward(nn.Module):
+    "Imple
