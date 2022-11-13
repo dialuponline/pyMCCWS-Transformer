3ce8faa16290963be977ae092da2ee9bde760fb0
dialuponline <dialuponline@proton.me>
2022-11-13 11:30:48 +0000

    rubocop

--- transformer.py
+++ transformer.py
@@ -82,4 +82,9 @@ class PositionwiseFeedForward(nn.Module):
         super(PositionwiseFeedForward, self).__init__()
         self.w_1 = nn.Linear(d_model, d_ff)
         self.w_2 = nn.Linear(d_ff, d_model)
-        self.dropout = n
+        self.dropout = nn.Dropout(dropout)
+
+    def forward(self, x):
+        return self.w_2(self.dropout(F.relu(self.w_1(x))))
+        
+class SublayerConnect
