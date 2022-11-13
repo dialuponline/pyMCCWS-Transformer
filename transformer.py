16ba2444bdc5fba0c30bf36d3474bfe02dd5f2bb
dialuponline <dialuponline@proton.me>
2022-11-13 06:49:27 +0000

    fix git ignore

--- transformer.py
+++ transformer.py
@@ -79,4 +79,7 @@ class LayerNorm(nn.Module):
 class PositionwiseFeedForward(nn.Module):
     "Implements FFN equation."
     def __init__(self, d_model, d_ff, dropout=0.1):
-        super(PositionwiseFeedForward, sel
+        super(PositionwiseFeedForward, self).__init__()
+        self.w_1 = nn.Linear(d_model, d_ff)
+        self.w_2 = nn.Linear(d_ff, d_model)
+        self.dropout = n
