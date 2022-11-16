68bd0b257c67b30de9d58aaa92f785151c0523e8
dialuponline <dialuponline@proton.me>
2022-11-16 19:51:20 +0000

    scrutinizer fixes

--- transformer.py
+++ transformer.py
@@ -98,3 +98,5 @@ class SublayerConnection(nn.Module):
         self.dropout = nn.Dropout(dropout)
 
     def forward(self, x, sublayer):
+        "Apply residual connection to any sublayer with the same size."
+        return x + self.dropout(sublayer(self.norm
