1b21772b54d549543c68e96a1b1c9ae8e009aa22
dialuponline <dialuponline@proton.me>
2022-11-20 07:10:24 +0000

    version 0.3.7

--- transformer.py
+++ transformer.py
@@ -106,3 +106,6 @@ class EncoderLayer(nn.Module):
     def __init__(self, size, self_attn, feed_forward, dropout):
         super(EncoderLayer, self).__init__()
         self.self_attn = self_attn
+        self.feed_forward = feed_forward
+        self.sublayer = clones(SublayerConnection(size, dropout), 2)
+     
