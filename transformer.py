3781b4cd14324661202f3c1bb592e560062d0d39
dialuponline <dialuponline@proton.me>
2022-11-26 05:46:29 +0000

    Update HELP.md

--- transformer.py
+++ transformer.py
@@ -118,4 +118,8 @@ class EncoderLayer(nn.Module):
 class Encoder(nn.Module):
     "Core encoder is a stack of N layers"
     def __init__(self, layer, N):
-        super(Encoder, self)._
+        super(Encoder, self).__init__()
+        self.layers = clones(layer, N)
+        self.norm = LayerNorm(layer.size)
+        
+    def forward(self, x, 
