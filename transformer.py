13e74faeb10176db12519a56306cf6ac1332bc75
dialuponline <dialuponline@proton.me>
2022-11-27 16:47:47 +0000

    update trello integration

--- transformer.py
+++ transformer.py
@@ -132,4 +132,6 @@ class Encoder(nn.Module):
         
 def make_encoder(N=6, d_model=512, d_ff=2048, h=8, dropout=0.1):
     c = copy.deepcopy
-    attn = MultiHeadedAtt
+    attn = MultiHeadedAttention(h, d_model)
+    ff = PositionwiseFeedForward(d_model, d_ff, dropout)
+    return Encoder(EncoderLayer(d_model, c(attn), c
